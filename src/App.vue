af48e65d0f79fa105c3df2665c3acb4f0748cc91
项嘉城 <jiacheng.xiang@jfjun.com>
2019-04-13 15:37:51 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{msg}}
+    <router-view/>
+    <el-button>默认按钮</el-button>
+      
+    <HelloWorld
+      need="111"
+    ></HelloWorld>
+  </div>
+</template>
+
+<script lang="ts">
+
+import {Vue,Component} from 'vue-property-decorator'
+import HelloWorld from "@/components/HelloWorld.vue";
+
+
+@Component({
+
+  name: 'app',
+
+  components:{
+    HelloWorld
+  }
+
+})
+
+export default class App extends Vue {
+
+  msg = '欢迎来到vue'
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{need}}</h2>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+import {Vue,Component, Prop} from 'vue-property-decorator'
+
+@Component({})
+export default class HelloWorld extends Vue {
+    msg:string = 'Welcome to Your Vue.js App'
+    @Prop(Number) readonly need!: number
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
